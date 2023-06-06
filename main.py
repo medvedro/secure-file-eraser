1ec8dd1a24ca60ed57cf23327cc5c81a25811765
medved <107792221+medvedro@users.noreply.github.com>
2023-06-06 20:32:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import shutil
 def secure_erase_file(file_path):
     file_size = os.path.getsize(file_path)
 
-    with open(file_path, 'r+b') as file_handle:
+    with open(file_path, 'rb+') as file_handle:
         for _ in range(3):  # Perform 3 overwrites
             for _ in range(file_size):
                 file_handle.seek(0)
@@ -36,43 +36,46 @@ def secure_erase_directory(directory):
             shutil.rmtree(dir_path)
             print("Removed directory - >", dir_path.lower())
 
-def secure_erasing(erasing_directory):
+def secure_erase_drive(drive_path):
     file_count = 0
     erased_file_count = 0
 
-    for root, dirs, files in os.walk(erasing_directory):
+    for root, dirs, files in os.walk(drive_path):
         file_count += len(files)
 
-    for root, dirs, files in os.walk(erasing_directory):
+    for root, dirs, files in os.walk(drive_path):
         for file_name in files:
             file_path = os.path.join(root, file_name)
             secure_erase_file(file_path)
             erased_file_count += 1
             print(f"Erased: {erased_file_count}/{file_count} files", end='\r')
 
-    secure_erase_directory(erasing_directory)
+    secure_erase_directory(drive_path)
 
 def main():
     while True:
-        erasing_directory = input("[ + ] Folder Path : ")
-        if os.path.isdir(erasing_directory):
+        erasing_path = input("[ + ] Drive or Directory Path: ")
+        if os.path.exists(erasing_path):
             break
         else:
-            print("Invalid directory path. Please enter a valid directory.")
+            print("Invalid path. Please enter a valid drive or directory path.")
 
     process_count = multiprocessing.cpu_count() 
 
     print("\n[ - ] Starting the erasing process...")
     pool = multiprocessing.Pool(processes=process_count)
-    pool.map(secure_erasing, [erasing_directory])
+    pool.map(secure_erase_drive, [erasing_path])
     pool.close()
     pool.join()
 
     print("\n[ + ] Erasing process completed.")
 
-    # Securely delete the directory itself
-    shutil.rmtree(erasing_directory)
-    print("Removed directory - >", erasing_directory.lower())
+    # Securely delete the drive or directory itself
+    if os.path.isdir(erasing_path):
+        shutil.rmtree(erasing_path)
+        print("Removed directory - >", erasing_path.lower())
+    else:
+        print("Secure erasure of drives is not supported in this script.")
 
 if __name__ == "__main__":
     main()
