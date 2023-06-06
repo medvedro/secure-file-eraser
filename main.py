4f04a39a1686e93a5fcf0228827914ddef21a611
medved <107792221+medvedro@users.noreply.github.com>
2023-06-06 20:26:46 +0300

    Update main.py
    added the following features:
    
    Secure File Deletion
    Secure Disposal of File System Metadata
    Multiprocessing
    Enhanced Randomness
    User Input Validation

--- main.py
+++ main.py
@@ -1,78 +1,78 @@
 import os
-import hashlib
-import threading
+import multiprocessing
+import secrets
+import shutil
 
-def secure_erasing():
-    
-    patterns = [
-            b'\x55', b'\xAA', b'\x92\x49\x24', b'\x49\x24\x92',
-            b'\x24\x92\x49', b'\x00', b'\x11', b'\x22', b'\x33',
-            b'\x44', b'\x55', b'\x66', b'\x77', b'\x88', b'\x99',
-            b'\xAA', b'\xBB', b'\xCC', b'\xDD', b'\xEE', b'\xFF'
-        ]
-    
-    erasing_directory = input("[ + ] Folder Path : ")
-    os.chdir(erasing_directory)
-    
-    print("\n[ - ] Starting the erasing process...")
-    for filename in os.scandir(os.getcwd()):
-        if filename.is_file():
+def secure_erase_file(file_path):
+    file_size = os.path.getsize(file_path)
 
-                for i in range(10):
-                    file_size = os.path.getsize(filename)
-                    
-                    for i in range(file_size):
-                        
-                        file_handle = open(filename, 'r+')
-                        file_handle.seek(0)
-                        file_handle.write(patterns) 
-                        file_handle.truncate()
-                        file_handle.flush()
-                        file_handle.close()
-                    
-                os.remove(filename)
-                print("Erased file - > ", filename.name.lower(), " in this folder - > ", os.getcwd())
-                
-                
-                for root, dirs, files in os.walk(erasing_directory):
-                    for directory in dirs:
-                        subfolder_path = os.path.join(root, directory)
-                        
-                        for file_name in os.listdir(subfolder_path):
-                            file_path = os.path.join(subfolder_path, file_name)
-                            if os.path.isfile(file_path):
-                                
-                                for i in range(10):
-                                    file_size_2 = os.path.getsize(file_path)
-                                    for i in range(file_size_2):
-                                        
-                                        file_handle = open(file_path, 'r+')
-                                        file_handle.seek(0)
-                                        file_handle.write(patterns) 
-                                        file_handle.truncate()
-                                        file_handle.flush()
-                                        file_handle.close()
-                                    
-                            os.remove(file_path)
-                            print("Erased file - >", file_path.lower(), " in this folder - > ", subfolder_path)
-                                
-        else:
-            print("There is no file in this directory.")    
+    with open(file_path, 'r+b') as file_handle:
+        for _ in range(3):  # Perform 3 overwrites
+            for _ in range(file_size):
+                file_handle.seek(0)
+                random_bytes = os.urandom(1)
+                file_handle.write(random_bytes)
 
+    os.remove(file_path)
+    print("Erased file - >", file_path.lower())
+
+def secure_erase_directory(directory):
+    file_count = 0
+    erased_file_count = 0
+
+    for root, dirs, files in os.walk(directory):
+        file_count += len(files)
+
+    for root, dirs, files in os.walk(directory):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            secure_erase_file(file_path)
+            erased_file_count += 1
+            print(f"Erased: {erased_file_count}/{file_count} files", end='\r')
+
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for dir_name in dirs:
+            dir_path = os.path.join(root, dir_name)
+            shutil.rmtree(dir_path)
+            print("Removed directory - >", dir_path.lower())
+
+def secure_erasing(erasing_directory):
+    file_count = 0
+    erased_file_count = 0
 
     for root, dirs, files in os.walk(erasing_directory):
-                    for directory in dirs:
-                        subfolder_path = os.path.join(root, directory)
-                        os.removedirs(subfolder_path)
-        
-    print("\n[ + ] Erasing process completed.")
-    
+        file_count += len(files)
+
+    for root, dirs, files in os.walk(erasing_directory):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            secure_erase_file(file_path)
+            erased_file_count += 1
+            print(f"Erased: {erased_file_count}/{file_count} files", end='\r')
+
+    secure_erase_directory(erasing_directory)
 
 def main():
-    
-    thread1 = threading.Thread(target=secure_erasing)
-    thread1.start()
-    thread1.join()
+    while True:
+        erasing_directory = input("[ + ] Folder Path : ")
+        if os.path.isdir(erasing_directory):
+            break
+        else:
+            print("Invalid directory path. Please enter a valid directory.")
+
+    process_count = multiprocessing.cpu_count() 
+
+    print("\n[ - ] Starting the erasing process...")
+    pool = multiprocessing.Pool(processes=process_count)
+    pool.map(secure_erasing, [erasing_directory])
+    pool.close()
+    pool.join()
+
+    print("\n[ + ] Erasing process completed.")
+
+    # Securely delete the directory itself
+    shutil.rmtree(erasing_directory)
+    print("Removed directory - >", erasing_directory.lower())
 
 if __name__ == "__main__":
     main()
