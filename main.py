2f83fc1f1d7af0f6493c3e71abffbe875dd392df
medved <107792221+medvedro@users.noreply.github.com>
2023-06-06 18:11:19 +0300

    Update main.py
    overwriting using 10 passes

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ def secure_erasing():
     erasing_directory = input("[ + ] Folder Path : ")
     os.chdir(erasing_directory)
     
-    print("\nStarting the erasing process...")
+    print("\n[ - ] Starting the erasing process...")
     for filename in os.scandir(os.getcwd()):
         if filename.is_file():
 
-                file_handle = open(filename, 'r+')
-                file_handle.seek(0)
-                file_handle.write("xxxxxxxxxxxxxxxxxxxxxxxxx") 
-                file_handle.truncate()
-                file_handle.close()
-                os.remove(filename)    
-                
+                for i in range(10):
+                    file_handle = open(filename, 'r+')
+                    file_handle.seek(0)
+                    file_handle.write("xxxxxxxxxxxxxxxxxxxxxxxxx") 
+                    file_handle.truncate()
+                    file_handle.close()
+                    
+                os.remove(filename)
                 print("Erased file - > ", filename.name.lower(), " in this folder - > ", os.getcwd())
+                
                 
                 for root, dirs, files in os.walk(erasing_directory):
                     for directory in dirs:
@@ -27,17 +29,16 @@ def secure_erasing():
                             file_path = os.path.join(subfolder_path, file_name)
                             if os.path.isfile(file_path):
                                 
-                                file_handle = open(file_path, 'r+')
-                                file_handle.seek(0)
-                                file_handle.write("xxxxxxxxxxxxxxxxxxxxxxxxx") 
-                                file_handle.truncate()
-                                file_handle.close()
+                                for i in range(10):
+                                    file_handle = open(file_path, 'r+')
+                                    file_handle.seek(0)
+                                    file_handle.write("xxxxxxxxxxxxxxxxxxxxxxxxx") 
+                                    file_handle.truncate()
+                                    file_handle.close()
+                                    
+                            os.remove(file_path)
+                            print("Erased file - >", file_path.lower(), " in this folder - > ", subfolder_path)
                                 
-                                os.remove(file_path)
-                                print("Erased file - >", file_path.lower(), " in this folder - > ", subfolder_path)
-                                
-                
-                
         else:
             print("There is no file in this directory.")    
 
